826d612916e0b12c1203b4e3c849aeb206231d18
hogdeer <49504279+hogdeer@users.noreply.github.com>
2025-11-11 16:28:53 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+
+import Cookies from 'js-cookie'
+
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import store from './store'
+import router from './router'
+
+import i18n from './lang' // Internationalization
+import './icons' // icon
+import './errorLog' // error log
+import './permission' // permission control
+import './mock' // simulation data
+
+import * as filters from './filters' // global filters
+
+import VxeUIAll from 'vxe-pc-ui'
+import 'vxe-pc-ui/lib/style.css'
+import VxeUITable from 'vxe-table'
+import 'vxe-table/lib/style.css'
+
+
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium', // set element-ui default size
+  i18n: (key, value) => i18n.t(key, value)
+})
+
+Vue.use(VxeUIAll)
+Vue.use(VxeUITable)
+// register global utility filters.
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  i18n,
+  render: h => h(App)
+})
